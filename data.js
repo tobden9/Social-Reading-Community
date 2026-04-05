ca65029e74d36302736884fb0950178e3d79da97
tobden <tshewangtobden10@gmail.com>
2026-04-05 12:44:11 +1000

    sprint 3 push

---
+++ data.js
@@ -0,0 +1,13 @@
+// data.js
+const bookData = [
+    { "id": "1", "title": "The Last Kingdom", "author": "Bernard Cornwell", "genre": "History", "tags": ["Historical Fiction", "Vikings", "War"] },
+    { "id": "2", "title": "Project Hail Mary", "author": "Andy Weir", "genre": "Sci-Fi", "tags": ["Space", "Science", "Survival"] },
+    { "id": "3", "title": "The Name of the Wind", "author": "Patrick Rothfuss", "genre": "Fantasy", "tags": ["Magic", "Coming of Age", "Music"] },
+    { "id": "4", "title": "The Silent Patient", "author": "Alex Michaelides", "genre": "Mystery", "tags": ["Psychological", "Suspense", "Thriller"] },
+    { "id": "5", "title": "Dune", "author": "Frank Herbert", "genre": "Sci-Fi", "tags": ["Politics", "Desert", "Epic"] },
+    { "id": "6", "title": "Circe", "author": "Madeline Miller", "genre": "History", "tags": ["Mythology", "Greece", "Fantasy"] },
+    { "id": "7", "title": "The Way of Kings", "author": "Brandon Sanderson", "genre": "Fantasy", "tags": ["Epic", "High Fantasy", "Knights"] },
+    { "id": "8", "title": "The 7 Deaths of Evelyn Hardcastle", "author": "Stuart Turton", "genre": "Mystery", "tags": ["Time Loop", "Whodunnit", "Creative"] },
+    { "id": "9", "title": "The Pillars of the Earth", "author": "Ken Follett", "genre": "History", "tags": ["Cathedrals", "Medieval", "Drama"] },
+    { "id": "10", "title": "Neuromancer", "author": "William Gibson", "genre": "Sci-Fi", "tags": ["Cyberpunk", "Hacking", "AI"] }
+];
